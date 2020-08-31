ab9aff3a6c1ec794e38b9daa3f3cb25cca7a2485
munkyuhwan <munkyuhwan@gmail.com>
2020-09-01 02:02:34 +0900

    수정

--- app/src/main/java/com/anji/babydiary/MainActivity.kt
+++ app/src/main/java/com/anji/babydiary/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.anji.babydiary
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.anji.babydiary.database.mainFeed.MainFeed
-import com.anji.babydiary.event.EventActivity
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.anji.babydiary.mainFeed.MainFeedActivity
-import com.anji.babydiary.myPage.MyPage
-import com.anji.babydiary.shopping.ShoppingActivity
-import com.anji.babydiary.tips.TipActivity
 import kotlinx.coroutines.*
+import java.util.*
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //month -1
+        val expDate = GregorianCalendar(2020, 8, 9) // midnight
+        val now = GregorianCalendar()
+
+        val isExpired = now.after(expDate)
+
+        if (isExpired) {
+            Toast.makeText(this, "사용기간이 만료되었습니다.", Toast.LENGTH_SHORT).show()
+            finishAffinity()
+            finish()
+            exitProcess(-1)
+        }
+
 
         var job = Job()
         var uiScope = CoroutineScope(Dispatchers.Main + job)
