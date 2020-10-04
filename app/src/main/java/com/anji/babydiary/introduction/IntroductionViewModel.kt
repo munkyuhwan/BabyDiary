0801624e571f25f37b5f6f5f7d24b39f05350168
munkyuhwan <munkyuhwan@gmail.com>
2020-10-04 21:51:02 +0900

    삭제 버튼

--- app/src/main/java/com/anji/babydiary/introduction/IntroductionViewModel.kt
+++ app/src/main/java/com/anji/babydiary/introduction/IntroductionViewModel.kt
@@ -4,15 +4,21 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import com.anji.babydiary.common.CommonCode
+import kotlin.random.Random
 
 class IntroductionViewModel(application: Application): AndroidViewModel(application) {
 
     var selectedSentence:String = ""
+    val random = Random
 
     init {
-        var rnd = Math.abs( (Math.random()%CommonCode.INTRODUCTION.size)*10 )
+        Random(5)
+        var rnd = rand(0,5)
         Log.e("dddd","randnum: ${rnd}")
-        selectedSentence = CommonCode.INTRODUCTION[rnd.toInt()]
+        selectedSentence = CommonCode.INTRODUCTION[rnd]
     }
 
+    fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
+    }
 }
